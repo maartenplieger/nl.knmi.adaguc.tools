0bd78d7fe58c5f573160b25de5bec18b15793be5
Maarten Terpstra <maarten.terpstra@knmi.nl>
2018-04-18 10:49:19 +0200

    Use the correct package and include it in the pom

--- src/main/java/nl/knmi/adaguc/tools/MyXMLParser.java
+++ src/main/java/nl/knmi/adaguc/tools/MyXMLParser.java
@@ -27,7 +27,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 
 public class MyXMLParser {
